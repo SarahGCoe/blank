4d1be387875936198e2ab527e347278dab2a942d
Sarah GHANIMI <sarah.ghanimi@gmail.com>
2019-05-28 14:52:51 +0200

    problem controller + dashboard controller + user controller + counter cache fonctionnel + views créées + routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @problems = Problem.order('problem_votes_count DESC').first(6)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'problems#home'
-  # namespace 'my' do
-  #   GET 'my/problems', to: 'problems#index'
-  # end
-  resources :problems, only: [:index, :show, :update] do
-    resources :solutions, only: [:index, :new, :create]
+  root to: 'pages#home'
+
+  get '/dashboard', to: 'users#dashboard'
+  resources :problems, only: [:index, :show] do
+    resources :solutions, only: [:new, :create]
   end
+
   resources :solutions, only: [:show]
 end
