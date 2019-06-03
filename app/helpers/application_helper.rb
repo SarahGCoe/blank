5e7fdd69ed830aec97d471df980b032f0982711b
Manon Roux <manon.roux@live.fr>
2019-06-03 13:53:49 +0200

    filters under progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
+# A specific form builder to deal with filtering attributes out based on parameters
+class StrongParametersFormBuilder < SimpleForm::FormBuilder
+  def input(attribute_name, *args, &block)
+    display_filter = self.options[:display_only]
+
+    if display_filter
+      super if display_filter.include?(attribute_name)
+    else
+      super
+    end
+  end
+end
+
 module ApplicationHelper
+  # Form Helper to be used
+  def filtered_form_for(object, options = {}, &block)
+    simple_form_for(object, options.merge(:builder => StrongParametersFormBuilder), &block)
+  end
 end
