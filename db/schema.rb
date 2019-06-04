65ec07dbfc04cd7046ea5d6d19450b723e9172a3
Sarah GHANIMI <sarah.ghanimi@gmail.com>
2019-06-04 18:35:26 +0200

    reglage de la div en double sur pb-card

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_06_04_130656) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,7 +61,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_130656) do
     t.datetime "updated_at", null: false
     t.integer "solution_votes_count", default: 0
     t.string "target"
-    t.string "file"
     t.string "photo"
     t.index ["problem_id"], name: "index_solutions_on_problem_id"
     t.index ["user_id"], name: "index_solutions_on_user_id"
